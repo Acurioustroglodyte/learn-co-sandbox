b0e539f2b2b6f736227545251c24927f09a75bc9
Trevor Jameson <tjameson15@gmail.com>
2018-10-02 17:34:56 +0000

    automatically backed up by learn

---
+++ interpole.rb
@@ -0,0 +1,15 @@
+# How many people are coming to Beyonce's party?
+def b_party
+  num_of_attendees = 547
+  puts "There are #{num_of_attendees} people coming to Beyonce's birthday party."
+end
+
+b_party
+
+# Bio flamingos
+def flaming
+  answer = "flamingot"
+  puts "A group of flamingos is called a #{answer}."
+end
+
+flaming
